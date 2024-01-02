89f4e316fc56730d647d620d914c356c799e97bb
niwatoro <girlwithskyhairpin@gmail.com>
2024-01-02 21:41:26 +0900

    Headerを追加

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,44 @@
+import {
+  Navbar,
+  NavbarBrand,
+  NavbarContent,
+  NavbarItem,
+} from "@nextui-org/react";
+import styles from "@/styles/components/Header/index.module.css";
+import { Kaisei_HarunoUmi } from "next/font/google";
+import { industries } from "@/data/industry";
+import NextLink from "next/link";
+import path from "path";
+
+const kaiseiHarunoUmi = Kaisei_HarunoUmi({ subsets: [], weight: "700" });
+
+export const Header = () => {
+  return (
+    <>
+      <Navbar>
+        <NavbarBrand>
+          <NextLink
+            className={`${styles.logo} ${kaiseiHarunoUmi.className}`}
+            href={"/"}
+          >
+            ドコカネ
+          </NextLink>
+        </NavbarBrand>
+      </Navbar>
+      <Navbar className={styles["industry-container"]} height={"auto"}>
+        <NavbarContent className={styles["industry-content"]}>
+          {industries.map((industry, i) => (
+            <NavbarItem key={i}>
+              <NextLink
+                className={styles["industry-link"]}
+                href={path.join("/industry", industry.href)}
+              >
+                {industry.name}
+              </NextLink>
+            </NavbarItem>
+          ))}
+        </NavbarContent>
+      </Navbar>
+    </>
+  );
+};
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,10 +1,12 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 import { NextUIProvider } from "@nextui-org/react";
+import { Header } from "@/components/Header";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <NextUIProvider>
+      <Header />
       <Component {...pageProps} />
     </NextUIProvider>
   );
