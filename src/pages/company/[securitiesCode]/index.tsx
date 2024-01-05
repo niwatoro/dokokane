77efbba62b418a7701d91afc5a70466e5907cb90
niwatoro <girlwithskyhairpin@gmail.com>
2024-01-05 10:48:07 +0900

    ダミーデータの充実

--- src/pages/company/[securitiesCode]/index.tsx
+++ src/pages/company/[securitiesCode]/index.tsx
@@ -18,11 +18,8 @@ export default function Home() {
   return (
     <>
       <Head>
-        <title>ドコカネ</title>
-        <meta
-          name="description"
-          content="ドコカネ - お金はどこからどこへ行く？"
-        />
+        <title>ドコカネ - {company.name}</title>
+        <meta name="description" content={`ドコカネ - ${company.name}`} />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
--- src/sections/IndexSection/index.tsx
+++ src/sections/IndexSection/index.tsx
@@ -3,25 +3,34 @@ import { Layout } from "@/sections/components/Layout";
 import { PreviewCard } from "@/sections/IndexSection/components/PreviewCard";
 import { Heading } from "@/sections/components/Heading";
 import styles from "@/styles/sections/IndexSection/index.module.css";
+import { Company } from "@/scripts/types/company";
 
 const companies = [
   {
     securitiesCode: "9613",
     name: "株式会社ＮＴＴデータグループ",
     netSales: 2167083000000,
+    operatingIncome: 98469000000,
     averageAnnualSalary: 8670000,
     averageAge: 39,
     numberOfEmployees: 195106,
+    sourceTitle: "有価証券報告書－第35期(2022/04/01－2023/03/31)",
+    sourceUrl:
+      "https://disclosure2dl.edinet-fsa.go.jp/searchdocument/pdf/S100QYZA.pdf",
   },
   {
     securitiesCode: "6758",
     name: "ソニーグループ株式会社",
     netSales: 168964000000,
+    operatingIncome: 10900000000,
     averageAnnualSalary: 11018955,
     averageAge: 42.4,
     numberOfEmployees: 2445,
+    sourceTitle: "有価証券報告書－第35期(2022/04/01－2023/03/31)",
+    sourceUrl:
+      "https://disclosure2dl.edinet-fsa.go.jp/searchdocument/pdf/S100QYZA.pdf",
   },
-];
+] as Company[];
 
 export const IndexSection = () => {
   return (
