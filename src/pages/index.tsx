06f3e0a10cac67b674c806f1ece2775449a488d3
niwatoro <girlwithskyhairpin@gmail.com>
2024-01-17 02:42:05 +0900

    descriptionタグを更新

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -8,7 +8,13 @@ export default function Home() {
         <title>ドコカネ</title>
         <meta
           name="description"
-          content="ドコカネ - お金はどこからどこへ行く？"
+          content={`
+            ドコカネ
+            
+            会社では何円稼ぎましたか？
+            そのうち何円貰いましたか？
+            そこから何円使えますか？
+          `}
         />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
